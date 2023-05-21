39605030098e754ce6a39ce4656d71a3d0923852
Leandro Costa <lrc@ua.pt>
2023-05-21 16:47:57 +0100

    organização

--- js/payment.js
+++ js/payment.js
@@ -1,10 +1,10 @@
 var itemListHtml = ""
 var total = 0.0
-for (itemIdx in cart){
+for (itemIdx in cart) {
     item = cart[itemIdx]
     itemListHtml = itemListHtml + `
         <div class="item-cart" style="width: 400px; margin-top: 30px;">
-            <img src="icons/remove_shopping_cart.png" alt="Item 1 Image" width="40" height="40"  onclick="removeFromCart('${item[4]}')">
+            <img src="../images/icons/remove_shopping_cart.png" alt="Item 1 Image" width="40" height="40"  onclick="removeFromCart('${item[4]}')">
             <div class="item-details-material" style="margin-top: 0; margin-bottom: 0; margin-right: 10px; padding: 0;">
                 <h4 style="margin: 0; font-size: 30px; color: #6C63FF;">${item[0]}</h4>
                 <h4 style="margin: 0; font-size: 30px;">${item[1]}€</h4>
@@ -20,14 +20,14 @@ document.getElementById("nArtigos").innerHTML = String(cart.length) + " artigos"
 document.getElementById("subtotal").innerHTML = "Subtotal do carrinho: " + String(total) + "€"
 document.getElementById("total").innerHTML = "Total: " + String(total) + "€"
 
-function removeFromCart(id){
-    for (itemIdx in cart){
+function removeFromCart(id) {
+    for (itemIdx in cart) {
         item = cart[itemIdx]
-        if(item[4] == id){
+        if (item[4] == id) {
             cart.splice(itemIdx, 1)
             localStorage.setItem('cart', JSON.stringify(cart));
             window.location.reload()
         }
     }
-    
+
 }
